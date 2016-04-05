44b415ba450f2f59c717eb0b92b1fcebd426f834
basi <blastertx@gmail.com>
2016-04-05 12:28:03 +0200

    - session controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :post
   
-  root 'session#index'
+  root 'page#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
