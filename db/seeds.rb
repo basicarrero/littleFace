f780e76838c635846bfdb0fd883c8007f1b1ca81
basi <blastertx@gmail.com>
2016-05-05 21:39:35 +0200

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,10 @@
 
 User.create(name: 'root', email: 'root@here.com', password: 'superuser', password_confirmation: 'superuser')
 
-User.create(name: 'Bender', email: 'bender@gmail.com', password: 'topsecret', password_confirmation: 'topsecret', friends: [3,4,5])
+User.create(name: 'Bender', email: 'bender@gmail.com', password: 'topsecret', password_confirmation: 'topsecret', friends: [3,4])
 User.create(name: 'Leela', email: 'leela@gmail.com', password: 'topsecret', password_confirmation: 'topsecret', friends: [2,4])
-User.create(name: 'Fry', email: 'fry@hotmail.es', password: 'topsecret', password_confirmation: 'topsecret', friends: [2,3,5])
-User.create(name: 'Basi', email: 'basicarrero@hotmail.com', password: 'topsecret', password_confirmation: 'topsecret', friends: [2,4])
+User.create(name: 'Fry', email: 'fry@hotmail.es', password: 'topsecret', password_confirmation: 'topsecret', friends: [2,3])
 
-50.times { |i| Post.create(user_id: 2, title: "Post #{i + 1}", created_at: (50 - i + 1).days.ago, text: BetterLorem.p(2, false, false)) }
-50.times { |i| Post.create(user_id: 3, title: "Post #{i + 1}", created_at: (50 - i + 1).days.ago, text: BetterLorem.p(2, false, false)) }
-50.times { |i| Post.create(user_id: 4, title: "Post #{i + 1}", created_at: (50 - i + 1).days.ago, text: BetterLorem.p(2, false, false)) }
-50.times { |i| Post.create(user_id: 5, title: "Post #{i + 1}", created_at: (50 - i + 1).days.ago, text: BetterLorem.p(2, false, false)) }
-  
-Post.create(user_id: 5, title: "Liked Post", text: 'Popular one', likes: [2,3,4])
+20.times { |i| Post.create(user_id: 2, title: "Post #{i + 1}", created_at: (25 - i + 1).days.ago, text: BetterLorem.p(2, false, false)) }
+20.times { |i| Post.create(user_id: 3, title: "Post #{i + 1}", created_at: (25 - i + 1).days.ago, text: BetterLorem.p(2, false, false)) }
+20.times { |i| Post.create(user_id: 4, title: "Post #{i + 1}", created_at: (25 - i + 1).days.ago, text: BetterLorem.p(2, false, false)) }
