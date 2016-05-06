82d084aab6efec176f82ea6c80ec9cd40f773e5b
basi <blastertx@gmail.com>
2016-05-06 02:18:12 +0200

    Seeds for deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,22 @@
 
 User.create(name: 'root', email: 'root@here.com', password: 'superuser', password_confirmation: 'superuser')
 
+
 User.create(name: 'Bender', email: 'bender@gmail.com', password: 'topsecret', password_confirmation: 'topsecret', friends: [3,4])
 User.create(name: 'Leela', email: 'leela@gmail.com', password: 'topsecret', password_confirmation: 'topsecret', friends: [2,4])
 User.create(name: 'Fry', email: 'fry@hotmail.es', password: 'topsecret', password_confirmation: 'topsecret', friends: [2,3])
 
-20.times { |i| Post.create(user_id: 2, title: "Post #{i + 1}", created_at: (25 - i + 1).days.ago, text: BetterLorem.p(2, false, false)) }
-20.times { |i| Post.create(user_id: 3, title: "Post #{i + 1}", created_at: (25 - i + 1).days.ago, text: BetterLorem.p(2, false, false)) }
-20.times { |i| Post.create(user_id: 4, title: "Post #{i + 1}", created_at: (25 - i + 1).days.ago, text: BetterLorem.p(2, false, false)) }
+
+Post.create(user_id: 2, title: "Sexy mama", text: "Hey sexy mama, wanna kill all humans?")
+Post.create(user_id: 2, title: "Black jack, and hookers", text: "I'll build by own theme park. With black jack, and hookers. In fact, forget the park!")
+Post.create(user_id: 2, title: "Bite It", text: "Bite my shiny metal ass!", photos: ["bender@gmail.com/BenderAvatar_smhf7s","bender@gmail.com/futurama_characters_kjaz7k"])
+
+Post.create(user_id: 3, title: "Mutant language", text: "No, this isn't mutant language. We use a lot more profanity")
+Post.create(user_id: 3, title: "So cute", text: "Aww, he's so cute. Wait, no he isn't! It looks like Bender!")
+Post.create(user_id: 3, title: "Mrs. Queequeg", text: "Is there Mrs. Queequeg?", photos: ["leela@gmail.com/futurama_ship_mnjtvv","leela@gmail.com/mindless_worker_elzzrr"])
+
+Post.create(user_id: 4, title: "It's the future!", text: "My God!! It's the future! My parents, my co-workers, my girlfriend. I'll never see any of them again. YAHOOO!!!!")
+Post.create(user_id: 4, title: "Internet", text: "Wow, you got that off the internet! In my day the internet was only used to download pornography")
+Post.create(user_id: 4, title: "Smells like blue", text: "Hey, what smells like blue?", photos: ["fry@hotmail.es/Fry_100_coffee_xoljc7","fry@hotmail.es/tabs_jxmsvx"])
+  
+#20.times { |i| Post.create(user_id: 2, title: "Post #{i + 1}", created_at: (25 - i + 1).days.ago, text: BetterLorem.p(2, false, false)) }
