2865238813070680bef2ba194d102c8c61a4d4ad
basi <blastertx@gmail.com>
2016-05-06 04:17:21 +0200

    comment tests

--- app/assets/javascripts/angular/paginator.js
+++ app/assets/javascripts/angular/paginator.js
@@ -127,62 +127,62 @@ angular.module("lf.paginator", [])
 	}])
 	.directive('linkHandler',['$window', '$timeout', '$animate', function ($window, $timeout, $animate) {
 	    return {
-			controller : ['$scope', '$document', function($scope, $document) {
-		    	$scope.goTo = function(target) {
-		    		return $document.scrollToElementAnimated(target, 130);
-		    	};
-			}],
-			link : function($scope, $element, $attr) {
-				
-		    	var go = function (id, after) {
-		    		var t = after ? after : 500;
-					$timeout(function () { // Do after render
-						var target = document.getElementById('p-' + id);
-						if (target) { 
-							$scope.goTo(target).then(function(res) {
-								$animate.addClass(target, 'pulse').then(function() {
-									$timeout(function(){ 
-										$animate.removeClass(target, 'pulse'); 
-									}, 2000);
-							    });
-							});
-						}
-					}, t);
-	        	};
-	        	
-				$element.on('click', function(e) {
-					//if (e.stopPropagation) e.stopPropagation();
-					if (e.preventDefault) e.preventDefault();
-					var pathParts = $attr.href.split('#');
-					var currentPath = $window.location.pathname == '/' ? '/page/home' : $window.location.pathname;
-					if ((pathParts[0] == '') || (pathParts[0] == currentPath)) {
-						if (pathParts.length == 2 && /^p-/.test(pathParts[1])) {
-							var id = pathParts[1].split('-')[1];
-							var promise = $scope.showUntil(id);
-							if (promise)
-								promise.then(go(id));
-							else
-								go(id);
-						}
-					}else {
-						$window.location.href = pathParts[0] + '?go=' + pathParts[1];
-					}
-				});
-				
-		        if ($attr.goTo) {
-	        		var id = $attr.goTo.split('-')[1];
-	        		var listener = $scope.$watch("isBusy", function (val) {
-	        			if (val === false) {
-							var promise = $scope.showUntil(id);
-							if (promise)
-								promise.then(go(id, 2000));
-							else
-								go(id);
-							listener();
-	        			}
-	        		});
-	        	}
-			}
+//			controller : ['$scope', '$document', function($scope, $document) {
+//		    	$scope.goTo = function(target) {
+//		    		return $document.scrollToElementAnimated(target, 130);
+//		    	};
+//			}],
+//			link : function($scope, $element, $attr) {
+//				
+//		    	var go = function (id, after) {
+//		    		var t = after ? after : 500;
+//					$timeout(function () { // Do after render
+//						var target = document.getElementById('p-' + id);
+//						if (target) { 
+//							$scope.goTo(target).then(function(res) {
+//								$animate.addClass(target, 'pulse').then(function() {
+//									$timeout(function(){ 
+//										$animate.removeClass(target, 'pulse'); 
+//									}, 2000);
+//							    });
+//							});
+//						}
+//					}, t);
+//	        	};
+//	        	
+//				$element.on('click', function(e) {
+//					//if (e.stopPropagation) e.stopPropagation();
+//					if (e.preventDefault) e.preventDefault();
+//					var pathParts = $attr.href.split('#');
+//					var currentPath = $window.location.pathname == '/' ? '/page/home' : $window.location.pathname;
+//					if ((pathParts[0] == '') || (pathParts[0] == currentPath)) {
+//						if (pathParts.length == 2 && /^p-/.test(pathParts[1])) {
+//							var id = pathParts[1].split('-')[1];
+//							var promise = $scope.showUntil(id);
+//							if (promise)
+//								promise.then(go(id));
+//							else
+//								go(id);
+//						}
+//					}else {
+//						$window.location.href = pathParts[0] + '?go=' + pathParts[1];
+//					}
+//				});
+//				
+//		        if ($attr.goTo) {
+//	        		var id = $attr.goTo.split('-')[1];
+//	        		var listener = $scope.$watch("isBusy", function (val) {
+//	        			if (val === false) {
+//							var promise = $scope.showUntil(id);
+//							if (promise)
+//								promise.then(go(id, 2000));
+//							else
+//								go(id);
+//							listener();
+//	        			}
+//	        		});
+//	        	}
+//			}
 	    };
 	}])
 	.directive('reveal',['$timeout', function ($timeout) {
